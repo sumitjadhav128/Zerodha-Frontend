b92922588f63243175da701e2592cb3e1c817cfc
Sumit <sumitjadhav6262@gmail.com>
2026-05-22 20:45:02 +0530

    add comp removed

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -74,17 +74,6 @@ const Menu = () => {
               </p>
             </Link>
           </li>
-          <li>
-            <Link
-              style={{ textDecoration: "none" }}
-              to="/apps"
-              onClick={() => handleMenuClick(6)}
-            >
-              <p className={selectedMenu === 6 ? activeMenuClass : menuClass}>
-                Apps
-              </p>
-            </Link>
-          </li>
         </ul>
         <hr />
         <div className="profile" onClick={handleProfileClick}>
