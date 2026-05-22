701794b49828a41ed7411b34af5db392f75c5e53
Sumit <sumitjadhav6262@gmail.com>
2026-05-22 20:10:21 +0530

    axios fixed

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.post(`${process.env.REACT_APP_API_URL}/AllHoldings`).then((res) => {
+    axios.get(`${process.env.REACT_APP_API_URL}/allHoldings`).then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
